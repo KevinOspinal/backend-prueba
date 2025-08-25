5559da1bf271d529d2a22b089d78d37cce76afbd
kevinacevedo <desarrollador2@plafilm.com>
2025-08-24 21:55:09 -0500

    fix: habilitar CORS para frontend en vercel

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ origins = [
     "http://localhost",
     "http://localhost:8000",    
     "http://localhost:3000",
-    "http://127.0.0.1:8000"
-]
+    "http://127.0.0.1:8000",
+    "https://frontend-prueba-iota.vercel.app"
+
 
 app.add_middleware(
     CORSMiddleware,
